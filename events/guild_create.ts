e00216d413ba422f161b909988b808c4df383434
Faizur <166435454+FaizurXD@users.noreply.github.com>
2024-06-30 02:00:41 +0530

    Update guild_create.ts

--- events/guild_create.ts
+++ events/guild_create.ts
@@ -7,10 +7,10 @@ export default class {
 	async run(guild: Guild) {
 		setActivity(guild.client);
 		const embed = new EmbedBuilder()
-			.setTitle(`${emojis.list} Welcome to Pyrite Bot`)
-			.setDescription(`${emojis.reply1} Thank you for choosing **Pyrite Bot**, I will make sure to try my best to protect your server from raider's, spammer's and so much more.
+			.setTitle(`${emojis.list} Welcome to Faizur Utils`)
+			.setDescription(`${emojis.reply1} Thank you for choosing **Faizue Utils**, I will make sure to try my best to protect your server from raider's, spammer's and so much more.
         
-You can configure me on the dashboard below this message. Need more servers protected? Add me to any server you think needs protection!
+You can invite me. Need Your servers protected? You can purchase me from faizur and  Add me to any server you think needs protection!
         `)
 			.setColor(0x2b2d31);
 
