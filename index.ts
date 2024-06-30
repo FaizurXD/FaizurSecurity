4accdcc7b3dcec72028041f81b69336903ddba7b
Faizur <166435454+FaizurXD@users.noreply.github.com>
2024-06-30 17:11:26 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,13 +7,11 @@ import {
 	Routes,
 	SlashCommandBuilder,
 } from "discord.js";
-
 import { readdir } from "node:fs/promises";
 import { join } from "node:path";
 import Fastify from "fastify";
 import { z } from "zod";
 import { dir, setActivity } from "./utils.js";
-import './alive';
 
 const envVariables = z.object({
 	DISCORD_TOKEN: z.string(),
@@ -25,7 +23,6 @@ envVariables.parse(process.env);
 
 declare global {
 	namespace NodeJS {
-		// biome-ignore lint/suspicious/noEmptyInterface: Infering from zod's types.
 		interface ProcessEnv extends z.infer<typeof envVariables> {}
 	}
 }
@@ -100,6 +97,21 @@ const registerEvents = async () => {
 const server = Fastify();
 
 server.get("/", async () => "unauthorized!");
+
+const pingWebsite = () => {
+	const website = 'https://faizur.onrender.com';
+
+	fetch(website)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			console.log(`Website ${website} pinged successfully`);
+		})
+		.catch(error => console.error(`There was a problem with the ping for ${website}:`, error));
+};
+
+setInterval(pingWebsite, 30000);
 
 try {
 	await loadCommands();
